33a1ffd2888138334ce2d9a201ca7b651eb09e56
Dhairya Bakhai <dhairya118@gmail.com>
2020-03-13 01:41:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_jwt import JWT
+from items import Item, ItemList
+from security import authenticate,identity
+from user import User, UserRegister
+
+
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate,identity)
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  
+
+
+
+
